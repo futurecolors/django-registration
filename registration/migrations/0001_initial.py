7ad1e04c7cffa62108781fa4825367eddbdcb4e8
Ilya Baryshev <baryshev@gmail.com>
2013-04-19 14:05:38 +0400

    Added south migration

---
+++ registration/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'RegistrationProfile'
+        db.create_table('registration_registrationprofile', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], unique=True)),
+            ('activation_key', self.gf('django.db.models.fields.CharField')(max_length=40)),
+        ))
+        db.send_create_signal('registration', ['RegistrationProfile'])
+
+    def backwards(self, orm):
+        # Deleting model 'RegistrationProfile'
+        db.delete_table('registration_registrationprofile')
+
+    models = {
+        'registration.registrationprofile': {
+            'Meta': {'object_name': 'RegistrationProfile'},
+            'activation_key': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['registration']
